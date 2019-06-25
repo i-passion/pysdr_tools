cfba9f565ca730c26e62298f8a958f47f0c65b97
BG6WRI <bg6wri@gmail.com>
2019-06-26 00:07:59 +0800

    Update data_draw.py

--- data_draw.py
+++ data_draw.py
@@ -1,24 +1,28 @@
 import matplotlib.pyplot as plt
 
 power=[]
+time=[]
 flag = 0
 
 f = open('data','r')
-
+f.readline()
+line=f.readline()
+time0=float(line.split()[1])
 while(flag==0):
 	line = f.readline()
 	if(line!=""):
-		power.append(float(line.split()[1]))
+		time.append(float(line.split()[1])-time0)
+		power.append(float(line.split()[2]))
 	else:
 		flag=1
 
 f.close()
 
 
-plt.plot(power,'B') # plotting the spectrum
+plt.plot(time,power,'B') # plotting the spectrum
 plt.xlabel('Time()')
 plt.ylabel('Power')
-plt.ylim((0, 0.01))
+plt.ylim((0, 10))
 
 
 plt.show()
